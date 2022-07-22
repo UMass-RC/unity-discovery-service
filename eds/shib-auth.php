dc55f60470a2e4c7f3ef2b4d674befc4966854ed
hakasapl <hakansaplakog@gmail.com>
2022-07-22 22:43:31 +0000

    initial

---
+++ eds/shib-auth.php
@@ -0,0 +1,24 @@
+<?php
+require "../resources/autoload.php";
+
+require_once config::PATHS["templates"] . "/header.php";
+?>
+
+<h1><?php echo unity_locale::AUTH_HEADER_MAIN; ?></h1>
+<hr>
+
+<div id="idpSelect"></div>
+
+<script src="/js/eds/idpselect_config.js" type="text/javascript" language="javascript"></script>
+<script src="/js/eds/idpselect.js" type="text/javascript" language="javascript"></script>
+<link rel="stylesheet" type="text/css" href="/eds/idpselect.css">
+
+<style>
+label {
+    margin: 0;
+}
+</style>
+
+<?php
+require_once config::PATHS["templates"] . "/footer.php";
+?>
