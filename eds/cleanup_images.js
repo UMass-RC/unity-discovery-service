6bdde121bd889169739a78eaeef9a3af3a3cae8d
simonLeary42 <71396965+simonLeary42@users.noreply.github.com>
2024-06-06 09:54:53 -0400

    format repository with Prettier

--- eds/cleanup_images.js
+++ eds/cleanup_images.js
@@ -1,18 +1,18 @@
 var changed_images = [];
 
-$("img.IdPSelectIdPImg").on("error", function() {
-    let entityID = $(this).parent().parent().parent().attr("data-entityid");
-    entityID = entityID.replaceAll("/", ".");
-    entityID = entityID.replaceAll(":", ".");
+$("img.IdPSelectIdPImg").on("error", function () {
+  let entityID = $(this).parent().parent().parent().attr("data-entityid");
+  entityID = entityID.replaceAll("/", ".");
+  entityID = entityID.replaceAll(":", ".");
 
-    if ($.inArray(entityID, changed_images) != -1) {
-        return;
-    } else {
-        changed_images.push(entityID);
-    }
+  if ($.inArray(entityID, changed_images) != -1) {
+    return;
+  } else {
+    changed_images.push(entityID);
+  }
 
-    let imagePath = "missing_logos/" + entityID + ".png";
-    $(this).attr("src", imagePath);
-    $(this).removeAttr("width");
-    $(this).removeAttr("height");
+  let imagePath = "missing_logos/" + entityID + ".png";
+  $(this).attr("src", imagePath);
+  $(this).removeAttr("width");
+  $(this).removeAttr("height");
 });
